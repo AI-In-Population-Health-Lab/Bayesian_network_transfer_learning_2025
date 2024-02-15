39d533687d97299afc4eb3ccf480250a49c744ab
Johncxsong <chenxisong@hotmail.com>
2024-02-14 20:16:31 -0500

    test_start clean up

--- src/test_start.java
+++ src/test_start.java
@@ -3,13 +3,10 @@ public class test_start {
 
         // run example-------------------------------
         runnerBNTL runnerBNTL = new runnerBNTL();
-//        String [] res = {"/Users/johnsong/downloads/smile_license/License.java",
-//                "/Users/johnsong/Documents/Ye_lab/BNTL_source_code/000influenza_ac_topazac_slc_topazslc",
-//                "folder10_Influenza_CFS_acmodel_0810_topazac_size50_slcmodel_0810_topazslc.bif_targetDataSize_1000-IG",
-//                "unadjust","off","off"};
 
-        String[] res = {"/Users/johnsong/downloads/smile_license/License.java",
-                "/Users/johnsong/documents/test_bntl_jar/no_KL",
+
+        String[] res = {"./smile_license/License.java",
+                "./no_KL",
                 "FLU-SLC2AC-0809-transfer20090118-IG001-NB",
                 "unadjust","off","off"
         };
